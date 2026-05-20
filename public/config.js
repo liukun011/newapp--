8861551dc762c64b0b716bb216600a4a95bc9de9
guohh <guohh@asiainfo.com>
2026-05-20 18:58:08 +0800

    正式环境地址也是个绝对地址,使用相对地址上传文件出错了

--- public/config.js
+++ public/config.js
@@ -4,11 +4,11 @@
  */
 window.APP_CONFIG = {
   // 认证中心基础路径
-  // VITE_AUTH_BASE_URL: 'http://user.binarysee.com/auth/', // 正式环境
-  VITE_AUTH_BASE_URL: 'http://192.168.8.201:21000/auth/', // 测试环境
+  VITE_AUTH_BASE_URL: 'http://user.binarysee.com/auth/', // 正式环境
+  // VITE_AUTH_BASE_URL: 'http://192.168.8.201:21000/auth/', // 测试环境
   // VITE_AUTH_BASE_URL: 'http://192.168.8.200:20101/auth/', // 开发环境
   // 业务接口基础路径
   // VITE_API_BASE_URL: 'http://192.168.8.200:20101/report/', 开发环境
-  VITE_API_BASE_URL: 'http://192.168.8.201:20101/report/', // 测试环境
-  // VITE_API_BASE_URL: '/report/', // 正式环境
+  // VITE_API_BASE_URL: 'http://192.168.8.201:20101/report/', // 测试环境
+  VITE_API_BASE_URL: 'https://xiaoli.binarysee.com/report/', // 正式环境
 };
